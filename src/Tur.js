66614777da0a84b573539a85a7bcbfe934744d0d
Thorstein Thorrud <tthorrud@gmail.com>
2018-09-16 14:56:59 +0200

    Compress background

---
+++ src/Tur.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Tur = (props) => (
+    <div>Deiohi {props.header}</div>
+);
+
+export default Tur;
---
+++ src/turer.js
@@ -0,0 +1,9 @@
+const turer = {
+    blefjell: {
+        header: 'Ei tur',
+        intro: 'Dette er beskrivelse av turen',
+        innhold: 'phaspduifhsdbfjsjsjsj HEHRER KOMMER INNHOLDERTTTTT'
+    }
+};
+
+export default turer;
